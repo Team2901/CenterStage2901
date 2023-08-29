ef51e46b833b82a899730869ac828385f7ec47e4
setiar25313 <setiar25313@student.cghsnc.org>
2023-08-29 15:40:02 -0400

    added utilities and basic mecanum drive stuff

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/MecanumDriveHardware.java
@@ -0,0 +1,68 @@
+package org.firstinspires.ftc.teamcode.Hardware;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class MecanumDriveHardware {
+
+    public static final double TICKS_PER_MOTOR_REV = 751.8;
+    public static final double WHEEL_CIRCUMFERENCE = Math.PI * 3.78;
+    public static final double DRIVE_GEAR_RATIO = 1;
+    public static final double TICKS_PER_DRIVE_REV = TICKS_PER_MOTOR_REV * DRIVE_GEAR_RATIO;
+    public static final double TICKS_PER_INCH = TICKS_PER_DRIVE_REV / WHEEL_CIRCUMFERENCE;
+
+    public DcMotor backLeft;
+    public DcMotor backRight;
+    public DcMotor frontLeft;
+    public DcMotor frontRight;
+    public BNO055IMU imu;
+
+    public void init(HardwareMap hardwareMap){
+        // initialize motors
+        backLeft = hardwareMap.dcMotor.get("backLeft");
+        backRight = hardwareMap.dcMotor.get("backRight");
+        frontLeft = hardwareMap.dcMotor.get("frontLeft");
+        frontRight = hardwareMap.dcMotor.get("frontRight");
+
+        // set motor directions (so it doesn't perpetually rotate)
+        backLeft.setDirection(DcMotor.Direction.REVERSE);
+        backRight.setDirection(DcMotor.Direction.FORWARD);
+        frontLeft.setDirection(DcMotor.Direction.REVERSE);
+        frontRight.setDirection(DcMotor.Direction.FORWARD);
+
+        // stop motors during initialization
+        backLeft.setPower(0);
+        backRight.setPower(0);
+        frontLeft.setPower(0);
+        frontRight.setPower(0);
+
+        // reset encoder positions to 0
+        backLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frontRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        // set motors up to run with encoders
+        backLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        frontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        frontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        // set up imu
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.calibrationDataFile = "BNO055IMUCalibration.json";
+        parameters.loggingEnabled = true;
+        parameters.loggingTag = "IMU";
+        parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
+
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+        imu.initialize(parameters);
+    }
+
+
+}
+// pi*6
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/MecanumTeleOp.java
@@ -0,0 +1,43 @@
+package org.firstinspires.ftc.teamcode.TeleOp;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.Hardware.ImprovedGamepad;
+import org.firstinspires.ftc.teamcode.Hardware.MecanumDriveHardware;
+
+public class MecanumTeleOp extends OpMode {
+
+    MecanumDriveHardware robot = new MecanumDriveHardware();
+    ImprovedGamepad impGamepad1;
+
+    public double rightStickYVal;
+    public double rightStickXVal;
+    public double leftStickYVal;
+    public double leftStickXVal;
+    public double rotate;
+    public double speedMod = 0.5;
+    public double turnMod = 0.5;
+
+    @Override
+    public void init() {
+        impGamepad1 = new ImprovedGamepad(gamepad1, new ElapsedTime(), "gamepad1");
+    }
+
+    @Override
+    public void loop() {
+        impGamepad1.update();
+
+        leftStickXVal = impGamepad1.left_stick_x.getValue() * speedMod;
+        leftStickYVal = impGamepad1.left_stick_y.getValue() * speedMod;
+        rightStickXVal = impGamepad1.right_stick_x.getValue() * speedMod;
+        rightStickYVal = impGamepad1.right_stick_y.getValue() * speedMod;
+
+        rotate = rightStickXVal * turnMod;
+
+        robot.backLeft.setPower(leftStickYVal-leftStickXVal+rotate);
+        robot.frontLeft.setPower(leftStickYVal+leftStickXVal+rotate);
+        robot.backRight.setPower(leftStickYVal+leftStickXVal-rotate);
+        robot.frontRight.setPower(leftStickYVal-leftStickXVal-rotate);
+    }
+}
