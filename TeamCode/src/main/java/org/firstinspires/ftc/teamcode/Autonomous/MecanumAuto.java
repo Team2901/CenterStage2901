30fbdb9d18ed937219e3bdf5c38e1ffc99dc09fb
setiar25313 <setiar25313@student.cghsnc.org>
2023-09-29 21:18:16 -0400

    added auto strafing (i think i did it correctly)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/MecanumAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/MecanumAuto.java
@@ -16,7 +16,9 @@ public class MecanumAuto extends LinearOpMode {
         robot.init(this.hardwareMap);
         waitForStart();
 
-        moveInches(24);
+        strafe(-5);
+
+        moveInches(94);
     }
 
     private void moveInches(double inches){
@@ -25,6 +27,40 @@ public class MecanumAuto extends LinearOpMode {
         robot.frontLeft.setTargetPosition(robot.frontLeft.getCurrentPosition() + ticks);
         robot.frontRight.setTargetPosition(robot.frontRight.getCurrentPosition() + ticks);
         robot.backLeft.setTargetPosition(robot.backLeft.getCurrentPosition() + ticks);
+        robot.backRight.setTargetPosition(robot.backRight.getCurrentPosition() + ticks);
+
+        robot.frontLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        robot.frontRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        robot.backLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        robot.backRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        robot.frontLeft.setPower(0.5);
+        robot.frontRight.setPower(0.5);
+        robot.backLeft.setPower(0.5);
+        robot.backRight.setPower(0.5);
+
+        while(opModeIsActive() && (robot.frontLeft.isBusy() && robot.frontRight.isBusy() && robot.backLeft.isBusy() && robot.backRight.isBusy())){
+            telemetry.addData("Current Left Position", robot.frontLeft.getCurrentPosition());
+            telemetry.addData("Current Right Position", robot.frontRight.getCurrentPosition());
+        }
+
+        robot.frontLeft.setPower(0);
+        robot.frontRight.setPower(0);
+        robot.backLeft.setPower(0);
+        robot.backRight.setPower(0);
+
+        robot.frontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.frontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.backLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.backRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+
+    private void strafe(double inches){
+        int ticks = (int) (inches * robot.TICKS_PER_INCH);
+
+        robot.frontLeft.setTargetPosition(robot.frontLeft.getCurrentPosition() + ticks);
+        robot.frontRight.setTargetPosition(robot.frontRight.getCurrentPosition() - ticks);
+        robot.backLeft.setTargetPosition(robot.backLeft.getCurrentPosition() - ticks);
         robot.backRight.setTargetPosition(robot.backRight.getCurrentPosition() + ticks);
 
         robot.frontLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
