dea40f8c075559fd3c3c59a727c27c68e4fe207c
WilliamBagel <102871850+WilliamBagel@users.noreply.github.com>
2023-11-06 23:30:51 -0500

    Better prop position?, maybe faster and more accurate

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vision/ShapeDetectionBlue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vision/ShapeDetectionBlue.java
@@ -8,6 +8,7 @@ import org.opencv.core.Point;
 import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
+import org.opencv.imgproc.Moments;
 import org.openftc.easyopencv.OpenCvPipeline;
 
 import java.util.ArrayList;
@@ -22,6 +23,8 @@ public class ShapeDetectionBlue extends OpenCvPipeline {
 
     Mat lastImage = null;
     Rect rect;
+
+    private int centerX = -100;
 
     public Mat processFrame(Mat input) {
         List<MatOfPoint> blueContours = new ArrayList<>();
@@ -45,33 +48,42 @@ public class ShapeDetectionBlue extends OpenCvPipeline {
         Imgproc.rectangle(HSVImage, cropRect, new Scalar(64, 64, 64), 10);
 
         Mat bwImage = new Mat();
-        Core.inRange(HSVImage, new Scalar(440, 50, 50), new Scalar(470, 255, 255), bwImage);
+        Core.inRange(HSVImage, new Scalar(85, 30, 50), new Scalar(135, 255, 255), bwImage);
 
         Mat blurImg = bwImage;
-        Imgproc.medianBlur(bwImage, blurImg, 17);
+        Imgproc.medianBlur(bwImage, blurImg, 5);
 
         Imgproc.findContours(blurImg, blueContours, new Mat(), Imgproc.RETR_LIST, Imgproc.CHAIN_APPROX_SIMPLE);
 
-        Imgproc.drawContours(lastImage, blueContours, 1, new Scalar(0, 255, 255));
-
-        rect = Imgproc.boundingRect(bwImage);
-        Imgproc.rectangle(lastImage, rect, new Scalar(0, 255, 160), 2);
+        if(blueContours.size() > 0) {//prevent errors from 0 or 1 points
+            Imgproc.drawContours(lastImage, blueContours, -1, new Scalar(0, 255, 255));
+            rect = Imgproc.boundingRect(blurImg);
+            Imgproc.rectangle(lastImage, rect, new Scalar(0, 255, 160), 2);
+            MatOfPoint biggestMoP = blueContours.get(0);
+            for(int i = 0; i < blueContours.size();i++){
+                MatOfPoint mom = blueContours.get(i);
+                if(Imgproc.contourArea(mom) > Imgproc.contourArea(biggestMoP)){
+                    biggestMoP = mom;
+                }
+            }
+            Moments moment = Imgproc.moments(biggestMoP);
+            int cx = (int)(moment.get_m10()/moment.get_m00());
+            int cy = (int)(moment.get_m01()/moment.get_m00());
+            Imgproc.circle(lastImage, new Point(cx,cy), 6, new Scalar(255,126,255),-1);
+            centerX = cx;
+        }else {
+            rect = new Rect();
+        }
 
         Imgproc.line(lastImage, new Point(107,0), new Point(107,240), new Scalar(0, 0, 0));
         Imgproc.line(lastImage, new Point(214,0), new Point(214,240), new Scalar(0, 0, 0));
-        if(rect != null) {
-            telemetry.addData("x", rect.x);
-            telemetry.addData("y", rect.y);
-        }
+
         telemetry.update();
 
         return lastImage;
     }
 
     public double xCoord(){
-        if(rect != null) {
-            return rect.x;
-        }
-        return -100;
+        return centerX;
     }
 }
