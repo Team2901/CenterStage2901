5537a8fc837598deb9d28418cb9fd874d9496aac
WilliamBagel <102871850+WilliamBagel@users.noreply.github.com>
2024-04-05 17:01:58 -0400

    Better blinkin for 2 modules

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/Blinkin.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/Blinkin.java
@@ -3,90 +3,154 @@ package org.firstinspires.ftc.teamcode.util;
 import com.qualcomm.hardware.rev.RevBlinkinLedDriver;
 
 public class Blinkin {
-    public RevBlinkinLedDriver blinkin;
+    public RevBlinkinLedDriver blinkinLeft;
+    public RevBlinkinLedDriver blinkinRight;
+
+    private final int EMPTY_PATTERN = 29;
+
+    //must be > 1000ms for epilepsy
+    private final int blinkPeriod = 1100;
+
+    private final int WHITE = 97;
+    private final int YELLOW = 85;
+    private final int GREEN = 87;
+    private final int PURPLE = 95;
+
 
     public enum PixelColor {
-        GRAY,
         WHITE,
         YELLOW,
-        GREEN
+        GREEN,
+        PURPLE
     }
 
-    private int pixelColor1;
-    private int pixelColor2;
+    public enum PixelStatus {
+        REQUESTED,
+        HELD,
+        EMPTY
+    }
+
+    private int pixelLeft;
+    private int pixelRight;
+
+    private PixelStatus pixelLeftStatus;
+    private PixelStatus pixelRightStatus;
 
     private long startTime;
-    private int blinkTime = 500;
-    private int restTime = 1000;
 
-    public Blinkin(RevBlinkinLedDriver blinkin) {
-        this.blinkin = blinkin;
-        blinkin.resetDeviceConfigurationForOpMode();
-        setPixel1(PixelColor.GRAY);
-        setPixel2(PixelColor.GRAY);
+    public Blinkin(RevBlinkinLedDriver blinkinLeft, RevBlinkinLedDriver blinkinRight) {
+        this.blinkinLeft = blinkinLeft;
+        this.blinkinRight = blinkinRight;
+
+        blinkinLeft.resetDeviceConfigurationForOpMode();
+        blinkinRight.resetDeviceConfigurationForOpMode();
+        setPixelStatus(PixelStatus.EMPTY);
+
         startTime = System.currentTimeMillis();
+//        setPixelLeft(PixelColor.GRAY);
+//        setPixelRight(PixelColor.GRAY);
     }
 
-    public void setBlinkTime(int blinkTime){
-        this.blinkTime = blinkTime;
-    }
+//    public void setBlinkTime(int blinkTime) {
+//        this.blinkTime = blinkTime;
+//    }
+//
+//    public void setRestTime(int restTime) {
+//        this.restTime = restTime;
+//    }
 
-    public void setRestTime(int restTime){
-        this.restTime = restTime;
-    }
-
-    public void setPixel1(PixelColor color) {
+    public void setPixelLeft(PixelColor color) {
         switch (color) {
-            case GRAY: {
-                pixelColor1 = 98;
-                break;
-            }
             case WHITE: {
-                pixelColor1 = 97;
+                pixelLeft = WHITE;
                 break;
             }
             case YELLOW: {
-                pixelColor1 = 85;
+                pixelLeft = YELLOW;
                 break;
             }
             case GREEN: {
-                pixelColor1 = 87;
+                pixelLeft = GREEN;
+                break;
+            }
+            case PURPLE: {
+                pixelLeft = PURPLE;
                 break;
             }
         }
     }
 
-    public void setPixel2(PixelColor color) {
+    public void setPixelRight(PixelColor color) {
         switch (color) {
-            case GRAY: {
-                pixelColor2 = 98;
-                break;
-            }
             case WHITE: {
-                pixelColor2 = 97;
+                pixelRight = WHITE;
                 break;
             }
             case YELLOW: {
-                pixelColor2 = 85;
+                pixelRight = YELLOW;
                 break;
             }
             case GREEN: {
-                pixelColor2 = 87;
+                pixelRight = GREEN;
+                break;
+            }
+            case PURPLE: {
+                pixelRight = PURPLE;
                 break;
             }
         }
     }
 
+    public void setPixelStatus(PixelStatus status){
+        setPixelLeftStatus(status);
+        setPixelRightStatus(status);
+    }
 
+    public void setPixelLeftStatus(PixelStatus status) {
+        pixelLeftStatus = status;
+//        if (status == PixelStatus.EMPTY) {
+//            blinkinLeft.setPattern(RevBlinkinLedDriver.BlinkinPattern.fromNumber(EMPTY_PATTERN));
+//        }
+    }
+
+    public void setPixelRightStatus(PixelStatus status) {
+        pixelRightStatus = status;
+//        if (status == PixelStatus.EMPTY) {
+//            blinkinRight.setPattern(RevBlinkinLedDriver.BlinkinPattern.fromNumber(EMPTY_PATTERN));
+//        }
+    }
+
+    long elapsedTime;
+
+    private void colorSide(boolean left){
+        RevBlinkinLedDriver blinkinModule = left?blinkinLeft:blinkinRight;
+        PixelStatus moduleStatus = left?pixelLeftStatus:pixelRightStatus;
+        int moduleColor = left?pixelLeft:pixelRight;
+
+        switch (moduleStatus){
+            case REQUESTED:{
+                if(elapsedTime < blinkPeriod){
+                    blinkinModule.setPattern(RevBlinkinLedDriver.BlinkinPattern.fromNumber(moduleColor));
+                }else{
+                    blinkinModule.setPattern(RevBlinkinLedDriver.BlinkinPattern.BLACK);
+                }
+                break;
+            }
+            case HELD:{
+                blinkinModule.setPattern(RevBlinkinLedDriver.BlinkinPattern.fromNumber(moduleColor));
+                break;
+            }
+            case EMPTY:{
+                blinkinModule.setPattern(RevBlinkinLedDriver.BlinkinPattern.fromNumber(EMPTY_PATTERN));
+                break;
+            }
+        }
+    }
 
     public void update() {
-        long elapsedTime =( System.currentTimeMillis() - startTime) %(blinkTime * 2L + restTime);
-        if(elapsedTime < blinkTime){
-            blinkin.setPattern(RevBlinkinLedDriver.BlinkinPattern.fromNumber(pixelColor1));
-        }else if (elapsedTime < blinkTime * 2L){
-            blinkin.setPattern(RevBlinkinLedDriver.BlinkinPattern.fromNumber(pixelColor2));
-        }else{
-            blinkin.setPattern(RevBlinkinLedDriver.BlinkinPattern.BLACK);
-        }
+        elapsedTime =( System.currentTimeMillis() - startTime) %(blinkPeriod * 2L);
+
+        colorSide(true);
+        colorSide(false);
     }
 }
